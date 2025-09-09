cb27b5ef3e2884b37573bd9eba58cc28423cb6ac
Prerna Singh <singh.prerna489@gmail.com>
2025-09-09 19:15:42 +0530

    Calculator App

--- calculator.js
+++ calculator.js
@@ -1,48 +1,48 @@
+// Fetching HTML input  and error block
+let inputVal = document.getElementById("inputVal");
+let errorBlck = document.getElementById("error");
+let result = "";
 
-let inputVal = document.getElementById('inputVal');
-let errorBlck = document.getElementById('error');
-let result ='';
+// Defined operands used
+let operands = ["+", "/", "-", "*", "%", "="];
 
-let operands = ['+','/','-','*','%','='];
+// Appending values in calculator display and performing operations
+function appendInput(dispValue) {
+  //Calculator display value
+  let inputLength = inputVal.value.length;
 
-function appendInput(dispValue){
-
-    let inputLength =  inputVal.value.length;
-
-    try{
-        if(inputLength==0 && operands.includes(dispValue) == true){  
-            inputVal.value = '';
-            throw new Error("Invalid input");     
-            
-        }else{
-        
-            if(dispValue === '='){
-                result = eval(inputVal.value);
-                if(result!==  Infinity && !isNaN(result))
-                    inputVal.value = result
-                else 
-                    throw new Error("Can't divide by zero");
-            }
-            else{
-                //Remove leading zeros
-                if (/^0[0-9]/.test(inputVal.value)) {
-                    inputVal.value = inputVal.value.replace(/^0+/, '');
-                }
-                inputVal.value += dispValue;
-            }
-            errorBlck.style.display = 'none';
-            errorBlck.innerHTML ='';
-        }  
-    }  
-    catch(err){
-            
-            errorBlck.innerHTML = err.message;
-            errorBlck.style.display = 'inline-block';
-            inputVal.value = '';
-    } 
+  try {
+    if (inputLength == 0 && operands.includes(dispValue) == true) {
+      // if any operand entered or 0 throw error
+      inputVal.value = "";
+      throw new Error("Invalid input");
+    } else {
+      if (dispValue === "=") {
+        result = eval(inputVal.value);
+        // if result infinity or not a number throw error
+        if (result !== Infinity && !isNaN(result)) inputVal.value = result;
+        else throw new Error("Can't divide by zero");
+      } else {
+        //Remove leading zeros
+        if (/^0[0-9]/.test(inputVal.value)) {
+          inputVal.value = inputVal.value.replace(/^0+/, "");
+        }
+        inputVal.value += dispValue;
+      }
+      errorBlck.style.display = "none";
+      errorBlck.innerHTML = "";
+    }
+  } catch (err) {
+    // display errors
+    errorBlck.innerHTML = err.message;
+    errorBlck.style.display = "inline-block";
+    inputVal.value = "";
+  }
 }
 
-function clearInput(){
-    inputVal.value = "";
-    result = 0;
+// Function for clearing calculator display
+
+function clearInput() {
+  inputVal.value = "";
+  result = 0;
 }
